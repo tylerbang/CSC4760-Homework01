06bf5959d2c5fa6f2823071db70415035fdf8a51
tylerbang <tylerbang@protonmail.com>
2024-02-29 13:30:19 -0600

    Problem 2, written out, not tested yet

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -7,10 +7,15 @@ int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // set n and m, you can change these values
-  int n,m = 16;
+  int n,m = 32;
   // Make View
-  
+  Kokkos::View<double**> woah("woah", n, m);
   // set values to 1000 * i * j;
+  Kokkos::parallel_for(n, KOKKOS_LAMBDA (const int i) {
+    for (int j = 0; j < m; j++) {
+      woah(i,j) = 1000 * i * j;
+    }
+  });
   }
   Kokkos::finalize();
 }
