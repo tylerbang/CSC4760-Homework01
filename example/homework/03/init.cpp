120c44836f55b807fccfd8f13adb227ce3801351
tylerbang <tylerbang@protonmail.com>
2024-02-29 13:35:35 -0600

    Written out problem 3, have not tested yet

--- example/homework/03/init.cpp
+++ example/homework/03/init.cpp
@@ -1,15 +1,18 @@
 #include <Kokkos_Core.hpp>
 #include <cstdio>
+#include <iostream>
 
 // Declare a 5 ∗ 7 ∗ 12 ∗ n View
 
 int main(int argc, char* argv[]) {
+  int n = 24; // APR hehe
+
   Kokkos::initialize(argc, argv);
   {
   // Make View
-  
+  Kokkos::View<double****> who_dat("who_dat", 5, 7, 12, n);
   // print name
-  
+  std::cout << "name " << who_dat.label() << std::endl;
   }
   Kokkos::finalize();
 }
