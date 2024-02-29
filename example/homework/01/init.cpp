04eff06d30d37d2a2547a895dfb13af8d62725ce
tylerbang <tylerbang@protonmail.com>
2024-02-29 13:21:47 -0600

    Problem 1 Solved, not yet tested

--- example/homework/01/init.cpp
+++ example/homework/01/init.cpp
@@ -1,5 +1,6 @@
 #include <Kokkos_Core.hpp>
 #include <cstdio>
+#include <iostream>
 
 // Problem: Link and run program with Kokkos where you initialize a View and print out its name with the $.label()$ method.
 
@@ -7,8 +8,9 @@ int main(int argc, char* argv[]) {
   Kokkos::initialize(argc, argv);
   {
   // Make View
-  
+  Kokkos::View<double*> a("a", 10);
   // print name
+  std::cout << "a.label() = " << a.label() << std::endl;
   }
   Kokkos::finalize();
 }
