66394f4bc9a240e6d7a3e491224f0ebecf9dc537
tylerbang <tylerbang@protonmail.com>
2024-03-03 11:11:05 -0600

    testing purposes again

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -12,9 +12,9 @@ int main(int argc, char* argv[]) {
   Kokkos::View<int**> woah("woah", n, m);
   // set values to 1000 * i * j;
   Kokkos::parallel_for(n, KOKKOS_LAMBDA (const int i) {
-    Kokkos::parallel_for(m, KOKKOS_LAMBDA (const int j) {
-      woah(i,j) = 1000 * i * j;
-    });
+    for (int j = 0; j < m; j++) {
+      woah(i,j) = 16;
+    }
   });
   Kokkos::fence();
 
