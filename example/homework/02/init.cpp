d4a2b576f0fc8868e3870596d3080a79e5bedd55
tylerbang <tylerbang@protonmail.com>
2024-03-03 11:57:00 -0600

    Printing out the whole thing now

--- example/homework/02/init.cpp
+++ example/homework/02/init.cpp
@@ -19,7 +19,11 @@ int main(int argc, char* argv[]) {
   });
   Kokkos::fence();
   // print out the values
-  std::cout << woah(9, 2) << std::endl;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      std::cout << woah(i,j) << " ";
+    }
+    std::cout << std::endl;
   }
   Kokkos::finalize();
 }
